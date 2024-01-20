012e2b7c5c1fed2a1580060336c3b4b1a17f8b4d
cgch8 <carloschie15@gmail.com>
2024-01-20 00:32:17 -0800

    made the javascript work

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 let counter = 0;
 let firstSelection = "";
 let secondSelection = "";
-let flippedCards = 0;
-let isGameStarted = false;
-let remainingTime = 100;
-let flipCount = 0;
-let timer;
 
-const cards = document.querySelectorAll(".cards .card")
-const startButton = document.getElementById("start-button")
-const remainingTimeElement = document.getElementById("remaining-time")
-const flipCountElement = document.getElementById("flip")
+const cards = document.querySelectorAll(".card");
+console.log(cards)
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    console.log("clicked")
+    card.classList.add("clicked");
 
-startButton.addEventListener("click", startGame)
+    console.log(card.classList)
+    console.log(card)
+    if (counter === 0) {
+      firstSelection = card.getAttribute("planet");
+      counter++;
+    } else {
+      secondSelection = card.getAttribute("planet");
+      counter = 0;
 
-cards.forEach((card)=> {
-    card.addEventListener("click", () => {
-        if (isGameStarted) {
-            flipCard(card)
-        }
-    })
-})
+      if (firstSelection === secondSelection) {
+        const correctCards = document.querySelectorAll(
+          ".card[planet='" + firstSelection + "']"
+        );
 
-function startGame() {
-    isGameStarted = true
-    startButton.disabled = 
-}
+        correctCards[0].classList.add("checked");
+        correctCards[0].classList.remove("clicked");
+        correctCards[1].classList.add("checked");
+        correctCards[1].classList.remove("clicked");
+      } else {
+        const incorrectCards = document.querySelectorAll(".card.clicked");
+
+        incorrectCards[0].classList.add("shake");
+        incorrectCards[1].classList.add("shake");
+
+        setTimeout(() => {
+          incorrectCards[0].classList.remove("shake");
+          incorrectCards[0].classList.remove("clicked");
+          incorrectCards[1].classList.remove("shake");
+          incorrectCards[1].classList.remove("clicked");
+        }, 800);
+      }
+    }
+  });
+});
